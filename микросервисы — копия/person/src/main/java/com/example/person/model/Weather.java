3311351b94ddd56dc67c8ad3543cb3be3f6b8e10
KATEKOY <145491056+KATEKOY@users.noreply.github.com>
2024-07-02 20:33:35 +0300

    Add files via upload

---
+++ микросервисы — копия/person/src/main/java/com/example/person/model/Weather.java
@@ -0,0 +1,32 @@
+package com.example.person.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Weather {
+    @NonNull
+    private MainInfo main;
+
+    @Data
+    public static class MainInfo {
+        @Id
+        @GeneratedValue
+        private int id;
+        private double temp;
+        private double feels_like;
+        private double temp_min;
+        private double temp_max;
+        private int pressure;
+        private int humidity;
+        private int sea_level;
+        private int grnd_level;
+    }
+
+}
